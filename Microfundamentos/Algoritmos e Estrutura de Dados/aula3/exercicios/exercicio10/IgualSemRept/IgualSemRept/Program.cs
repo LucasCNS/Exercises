db7bec1d462bc30c514a16a7557208feb79ff98f
LucasCNS <celionuvem@gmail.com>
2023-01-13 17:05:28 -0300

    DEV - Add condição para escolher entre For, Foreach ou Sair.

--- Microfundamentos/Algoritmos e Estrutura de Dados/aula3/exercicios/exercicio10/IgualSemRept/IgualSemRept/Program.cs
+++ Microfundamentos/Algoritmos e Estrutura de Dados/aula3/exercicios/exercicio10/IgualSemRept/IgualSemRept/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Globalization;
 
 //10 - Faça o método public ArrayList Intersecao(ArrayList al1, ArrayList al2) que recebe dois ArrayLists
 //e retorna outro contendo os elementos em comum armazenados em al1 e al2, sem repetição.
@@ -8,8 +7,8 @@ class Program
 {
 	static void Main(string[] args)
 	{
-		int qntdAl1 = 10;
-		int qntdAl2 = 20;
+		int qntdAl1 = 6;
+		int qntdAl2 = 10;
 
 		ArrayList al1 = new ArrayList();
 		for (int i = 0; i < qntdAl1; i++)
@@ -30,16 +29,45 @@ class Program
 		Console.WriteLine("\nArrayList 2\n");
 		Mostra(al2);
 
-		Console.WriteLine("\nArrayList Intersecao\n");
 		Mostra(Intersecao(al1, al2));
 
 		Console.ReadKey();
 	}
+	
 
 	public static ArrayList Intersecao(ArrayList al1, ArrayList al2)
 	{
 		ArrayList iguais = new ArrayList();
+		int condicao = 0;
+		Console.Write("\nDeseja usar o método '1-ForEach', '2-For' ou '3-Sair'? ");
+		condicao=int.Parse(Console.ReadLine());
 		
+		if (condicao == 1)
+		{
+			Console.WriteLine("\nArrayList Intersecao - ForEach\n");
+			IntersecaoForEach(al1, al2);
+
+		} else if (condicao == 2)
+		{
+			Console.WriteLine("\nArrayList Intersecao - For\n");
+			IntersecaoFor(al1, al2);
+
+		} else if(condicao == 3)
+		{
+			Console.WriteLine("Você escolheu sair.");
+			Console.ReadKey();
+		}
+		else if (condicao < 1 || condicao > 3)
+		{
+			Console.WriteLine("Valor inserido inválido!");
+		}
+
+		return al1;
+	}
+
+	static void IntersecaoForEach(ArrayList al1, ArrayList al2)
+	{
+		ArrayList iguais = new ArrayList();
 		foreach (var elemento in al2)
 		{
 			if (al1.Contains(elemento))
@@ -47,10 +75,22 @@ class Program
 				iguais.Add(elemento);
 			}
 		}
-
 		al1.Clear();
 		al1.AddRange(iguais);
-		return al1;
+	}
+
+	static void IntersecaoFor(ArrayList al1, ArrayList al2)
+	{
+		ArrayList iguais = new ArrayList();
+		for (int i = 0; i < al2.Count; i++)
+		{
+			if (al1.Contains(al2[i]))
+			{
+				iguais.Add(al2[i]);
+			}
+		}
+		al1.Clear();
+		al1.AddRange(iguais);
 	}
 
 	static void Mostra(ArrayList arraylist)
