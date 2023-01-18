723ca70c2bb461d1ae033125911762031b6c5aad
LucasCNS <celionuvem@gmail.com>
2023-01-18 15:15:29 -0300

    DEV - ADD exercício 13 e 14. CORRIGIDOS

---
+++ Microfundamentos/Algoritmos e Estrutura de Dados/aula3/exercicios/exercicio13/ALObject/ALObject/Program.cs
@@ -0,0 +1,62 @@
+﻿//13 - Faça o método public ArrayList OcorrenciasDe(ArrayList al, Object elemento) que recebe um ArrayList al e um Object elemento
+//e retorna outro ArrayList contendo todas as posições em que o elemento aparece em al.
+
+using System.Collections;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		ArrayList al = new ArrayList();
+		
+		al.Add(1);
+		al.Add(2);
+		al.Add(1);
+		al.Add(2);
+		al.Add(1);
+		al.Add(3);
+		al.Add(3);
+		al.Add(3);
+
+		Console.WriteLine("ArrayList\n");
+		Mostra(al);
+
+	
+		Object elemento = 2;
+		Console.WriteLine();
+
+		//Console.WriteLine($"Posições em que o item {elemento} se encontra.");
+		Mostra(OcorrenciasDe(al, elemento));
+	}
+
+	public static ArrayList OcorrenciasDe(ArrayList al, Object elemento)
+	{
+		ArrayList posicao = new ArrayList();
+
+		//foreach (var item in al)
+		//{
+		//	if (item.Equals(elemento))
+		//	{
+		//		posicao.Add(i);
+		//	}
+		//	i++;
+		//}
+
+		for (int i = 0; i < al.Count; i++)
+		{
+			if (al[i].Equals(elemento))
+			{
+				posicao.Add(i);
+			}
+		}
+		return posicao;
+	}
+
+	static void Mostra(ArrayList arraylist)
+	{
+		foreach (var item in arraylist)
+		{
+			Console.WriteLine($"{item}");
+		}
+	}
+}
