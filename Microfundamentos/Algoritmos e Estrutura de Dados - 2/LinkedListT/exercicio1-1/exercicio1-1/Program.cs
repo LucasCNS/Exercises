2560d9d519c82e31cfb5e8b419c2ee6f85d86fbe
LucasCNS <celionuvem@gmail.com>
2023-01-24 11:09:37 -0300

    DEV - Exercicio1-1, mostrar a quantidade de vezes que um elemento se repete na lista.

---
+++ Microfundamentos/Algoritmos e Estrutura de Dados - 2/LinkedListT/exercicio1-1/exercicio1-1/Program.cs
@@ -0,0 +1,48 @@
+﻿//Faça o método public int QtdeOcorrencias(List<String> origem, String elemento)
+//que retorna a quantidade de vezes que o elemento passado como parâmetro está armazenado no List<String> origem.
+
+using System.Collections;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		List<string> origem = new List<string>();
+		for (int i = 0; i < 5; i++)
+		{
+			origem.Add("Item" + i);
+		}
+		origem.Add("Item1");
+		origem.Add("Item1");
+
+		Console.WriteLine("List:\n");
+		Mostrar(origem);
+
+		string elemento = null;
+		Console.Write("\nDigite o item que procura: ");
+		elemento = Console.ReadLine();
+
+		QtdeOcorrencias(origem, elemento);
+	}
+
+	public static int QtdeOcorrencias(List<String> origem, String elemento)
+	{
+		int qtdAparece = 0;
+
+		for (int i = 0; i < origem.Count; i++)
+		{
+			if (origem[i].Equals(elemento)) qtdAparece ++;
+		}
+		
+		Console.WriteLine($"O número de vezes que o '{elemento}' aparece, é de {qtdAparece} vezes.");
+		return qtdAparece;
+	}
+
+	static void Mostrar(IEnumerable origem)
+	{
+		foreach (var item in origem)
+		{
+			Console.WriteLine($"{item}");
+		}
+	}
+}
