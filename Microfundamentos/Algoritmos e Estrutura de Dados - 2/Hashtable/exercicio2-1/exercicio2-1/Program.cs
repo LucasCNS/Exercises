8c3f0d8ed9de8fd80d9829dbce72b58e057118c1
LucasCNS <celionuvem@gmail.com>
2023-01-25 12:06:13 -0300

    DEV - Exercicio2-1

---
+++ Microfundamentos/Algoritmos e Estrutura de Dados - 2/Hashtable/exercicio2-1/exercicio2-1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+
+//1 - Faça o método public ArrayList CopiarQueueParaArrayList(Queue origem)
+//que copia todos os elementos do Queue origem e retorna um novo ArrayList.
+//Neste exercício não é permitido o uso dos métodos Clone() nem CopyTo().
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Queue origem = new Queue();
+
+		for (int i = 0; i < 5; i++)
+		{
+			origem.Enqueue(i + 1);
+		}
+		Console.WriteLine("Queue:");
+		Mostrar(origem);
+
+		Console.WriteLine("ArrayList:");
+		Mostrar(CopiarQueueParaArrayList(origem));	
+	}
+
+	static ArrayList CopiarQueueParaArrayList(Queue origem)
+	{
+		ArrayList list = new ArrayList();
+
+		foreach (var item in origem)
+		{
+			list.Add(item);
+		}
+		return list;
+	}
+
+	static void Mostrar(IEnumerable origem)
+	{
+		Console.WriteLine();
+		foreach (var item in origem)
+		{
+			Console.WriteLine($"{item}");
+		}
+	}
+
+	
+}
