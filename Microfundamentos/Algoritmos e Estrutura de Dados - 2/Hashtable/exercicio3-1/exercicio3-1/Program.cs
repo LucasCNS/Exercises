0d727110ee95d0341e6f0764d7306c9d242ce0db
LucasCNS <celionuvem@gmail.com>
2023-01-25 12:06:53 -0300

    DEV - Exercicio3-1

---
+++ Microfundamentos/Algoritmos e Estrutura de Dados - 2/Hashtable/exercicio3-1/exercicio3-1/Program.cs
@@ -0,0 +1,41 @@
+﻿//2 - Faça o método public Queue CopiarQueue(Queue origem)
+//que retorna uma cópia da Queue origem passada como parâmetro.
+//Os elementos da Queue origem devem permanecer na mesma ordem original.
+//Neste exercício é permitido o uso dos métodos Clone() e CopyTo() da classe Queue.
+
+using System.Collections;
+using System.Reflection.Metadata.Ecma335;
+
+class Program 
+{ 
+	static void Main(string[] args)
+	{
+		Queue origem = new Queue();
+		
+		for (int i = 0; i < 5; i++)
+		{
+			origem.Enqueue(i + 1);
+		}
+
+		Console.WriteLine("Queue origem");
+		Mostrar(origem);
+
+		Console.WriteLine("Queue copia da origem");
+		Mostrar(CopiarQueue(origem));
+	}
+
+	public static Queue CopiarQueue(Queue origem)
+	{
+		Queue copiaQueue = new Queue(origem);
+		return copiaQueue;
+	}
+
+	static void Mostrar(IEnumerable colecao)
+	{
+		foreach (var item in colecao)
+		{
+			Console.WriteLine($"{item}");
+		}
+	}
+	
+}
