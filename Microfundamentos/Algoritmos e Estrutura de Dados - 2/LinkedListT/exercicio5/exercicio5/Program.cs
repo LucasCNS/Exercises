a792f4e67df64653a35331c40c7593da0e167b1a
LucasCNS <celionuvem@gmail.com>
2023-01-25 12:04:35 -0300

    DEV - Exercicio5

---
+++ Microfundamentos/Algoritmos e Estrutura de Dados - 2/LinkedListT/exercicio5/exercicio5/Program.cs
@@ -0,0 +1,57 @@
+﻿//5 - Faça o método public void VaiProFundo<T>(Stack<T> origem, T elemento)
+//que empilha o elemento passado como parâmetro no fundo da Stack<T> origem, ao invés de empilhar no topo.
+
+using System.Collections;
+class Program
+{
+	static void Main(string[] args)
+	{
+		Stack<int> origem = new Stack<int>();
+		for (int i = 0; i < 7; i++)
+		{
+			origem.Push(i);
+		}
+		Console.WriteLine("Stack origem:\n");
+		Mostrar(origem);
+
+		int elemento = 10;
+
+		Console.WriteLine($"\nO valor que será jogado para o fundo da pilha é: {elemento}.\n");
+		VaiProFundo(origem, elemento);
+
+	}
+
+	public static void VaiProFundo<T>(Stack<T> origem, T elemento)
+	{
+		Stack<T> stackAux = new Stack<T>();
+		Stack<T> stackAux2 = new Stack<T>();
+
+		for (int i = origem.Count - 1; i >= 0 ; i--)
+		{
+			stackAux.Push(origem.Pop());
+		}
+
+		Console.WriteLine($"\nStack Aux:\n");
+		Mostrar(stackAux);
+
+		origem.Push(elemento);
+
+		for (int i = stackAux.Count - 1; i >= 0 ; i--)
+		{
+			origem.Push(stackAux.Pop());
+		}
+
+		Console.WriteLine($"\nStack Origem com elemento:\n");
+		Mostrar(origem);
+
+	}
+
+
+	static void Mostrar(IEnumerable objects)
+	{
+		foreach (var item in objects)
+		{
+			Console.WriteLine(item);
+		}
+	}
+}
