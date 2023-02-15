50ff8119a7f44f423184b048f0c1943445366b56
LucasCNS <celionuvem@gmail.com>
2023-02-15 14:43:46 -0300

    feat - Add classe Veiculo.

---
+++ Microfundamentos/Desenvolvimento De Aplicacoes Web Back-End/web-backend/web-backend/Models/Veiculo.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace web_backend.Models
+{
+	[Table("Veiculos")]
+	public class Veiculo
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required(ErrorMessage = "Obrigatório informar o nome.")]
+		public string Nome { get; set; }
+		[Required(ErrorMessage = "Obrigatório informar a placa.")]
+		public string Placa { get; set; }
+	}
+}
